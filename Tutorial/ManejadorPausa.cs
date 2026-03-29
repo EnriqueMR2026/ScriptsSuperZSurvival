0969e182fceefa8dd24d0868058e24c4e1f945e7
EnriqueMR2026 <enriquemorterareyes@gmail.com>
2026-03-28 22:04:17 -0600

    auto-commit

--- Tutorial/ManejadorPausa.cs
+++ Tutorial/ManejadorPausa.cs
@@ -19,4 +19,34 @@ public class ManejadorPausa : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
+
+    public void Reanudar()
+    {
+        estaPausado = false;
+        Time.timeScale = 1f;
+        panelPausa.SetActive(false);
+        panelConfiguracion.SetActive(false);
+
+        // Bloqueamos el ratón de nuevo para seguir disparando y moviendo la cámara
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void AbrirConfiguracion()
+    {
+        panelPausa.SetActive(false);
+        panelConfiguracion.SetActive(true);
+    }
+
+    public void IrAlMenuPrincipal()
+    {
+        // Es súper importante regresar el tiempo a la normalidad antes de cambiar de escena
+        Time.timeScale = 1f;
+
+        // Aquí guardamos la variable para que el Menú Principal sepa qué hacer al cargar
+        PlayerPrefs.SetInt("MostrarGameOver", 1);
+        PlayerPrefs.Save();
+
+        SceneManager.LoadScene("MenuPrincipal");
+    }
 }
